b1fed20234dd9041e0307954f2acdb9da6efe4f6
Miguel22247 <minipachru@gmail.com>
2021-10-01 14:05:46 -0300

    add console log on start

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const config = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on("ready", () => {
-  console.log(`Bienvenido de nuevo, soy el bot de Expreso Mercosur`);
-
+  console.log(`Watching Holberton School server with ${client.users.cache.size} members.`);
   client.user.setPresence({
       activity: {
           name: `The intranet | Watching Holberton School server with ${client.users.cache.size} members.`,
